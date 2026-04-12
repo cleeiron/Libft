577010b5a5186cb781adcb01191b36d8cd0f71f7
Cleeiron <pinchonclaire@gmail.com>
2026-04-12 15:21:39 +0200

    chill

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,35 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cleiron <cleiron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/08 17:32:07 by cleiron           #+#    #+#             */
-/*   Updated: 2026/04/12 12:51:33 by cleiron          ###   ########.fr       */
+/*   Created: 2026/04/12 12:52:46 by cleiron           #+#    #+#             */
+/*   Updated: 2026/04/12 15:20:54 by cleiron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include<stdio.h>
 
-char *ft_strchr(char *str, int c)
+
+char *ft_strrchr(char *str, int c)
 {
     int i = 0;
+    char *res = NULL;
 
     while(str[i] != '\0')
     {
         if(str[i] == c)
+        {
+           res = &str[i];
+        }
+        
+        if(c == '\0')
             return &str[i];
         i++;
     }
-    if(c == '\0')
-        return &str[i];
-    return 0;
+    return res;
 }
 
-int main()
-{  
-    printf("%s\n", ft_strchr("coucou", '\0'));
+int main ()
+{
+    printf("%s\n", ft_strrchr("coucou", 'u'));
 
     return 0;
 }

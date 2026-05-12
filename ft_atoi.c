144b8e609606c7f87810faaa017c5ac16569a113
Cleeiron <pinchonclaire@gmail.com>
2026-05-12 17:12:01 +0200

    bon vieux atoi dmc*

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,29 +6,45 @@
 /*   By: cleiron <cleiron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/13 21:25:26 by cleiron           #+#    #+#             */
-/*   Updated: 2026/04/13 21:41:37 by cleiron          ###   ########.fr       */
+/*   Updated: 2026/05/12 17:09:21 by cleiron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<unistd.h>
+#include<stdio.h>
 
 int ft_atoi(const char *str)
 {
     int i = 0;
-    int sign = 0;
+    int sign = 1;
+    int digit = 0;
+    int result = 0;
 
-    while(str[i] != '\0')
+    while(str[i] == 127 || (str[i] >= 0 && str[i] <= 40))
+        i++;
+        
+    while (str[i] == '-' || str[i] == '+')
     {
-        if(str[i] >= 0 && str[i] <= 32 || str[i] == 127)
-            i++;
-        
-        if(str[i] == 45 || str[i] ==)
-            write(1, '-', 1);
-        
-        if(str[i] >= 48 && str[i] <= 57)
-            write(1, )
-
-        
-        
+        if (str[i] == '-')
+            sign = sign * -1;
+        i++;
     }
+        
+    while(str[i] >= 48 && str[i] <= 57)
+        {
+            digit = str[i] - '0';
+            result = result * 10 + digit;
+            i++; 
+        }
+    return result *sign;
 }
+
+int main()
+{
+    char *str = "--236cc";
+
+    printf("%d\n", ft_atoi(str));
+
+    return 0;
+}
+    
+        

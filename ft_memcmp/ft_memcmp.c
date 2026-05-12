cc6bb4bf8a96b294096292665498739070eb6c13
Cleeiron <pinchonclaire@gmail.com>
2026-05-12 17:42:37 +0200

    tout rangé

--- ft_memcmp/ft_memcmp.c
+++ ft_memcmp/ft_memcmp.c
@@ -1,20 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   strnstr.c                                          :+:      :+:    :+:   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cleiron <cleiron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/13 20:19:35 by cleiron           #+#    #+#             */
-/*   Updated: 2026/04/13 20:22:48 by cleiron          ###   ########.fr       */
+/*   Created: 2026/04/13 19:58:13 by cleiron           #+#    #+#             */
+/*   Updated: 2026/05/12 17:33:17 by cleiron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<stdio.h>
 
-char *ft_strnstr(const char *s1, const char *s2, size_t n)
+int ft_memcmp(const void *s1, const void *s2, size_t n)
 {
     size_t i = 0;
 
-    
+    const unsigned char *s;
+    const unsigned char *c;
+    s = (const unsigned char *)s1;
+    c = (const unsigned char *)s2;
+
+    while(i < n)
+    {
+        
+        if(s[i] != c[i])
+            return s[i] - c[i];
+        i++;
+    }
+    return 0;
 }
